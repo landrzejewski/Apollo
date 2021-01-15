b9fdb468d6874a07b5534a68d59e55f775e315dc
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2021-01-15 19:20:51 +0100

    Prefix

---
+++ Sources/Apollo/Parsing/Prefix.swift
@@ -0,0 +1,57 @@
+public struct Prefix<Input, Output>: Parser where Input: Collection, Input.SubSequence == Input {
+    
+    public typealias Output = Input
+    
+    public let maxLength: Int?
+    public let minLength: Int
+    public let predicate: ((Input.Element) -> Bool)?
+    
+    public init(minLength: Int = 0, maxLength: Int? = nil, while predicate: @escaping (Input.Element) -> Bool) {
+        self.minLength = minLength
+        self.maxLength = maxLength
+        self.predicate = predicate
+    }
+    
+    public init(_ length: ClosedRange<Int>, while predicate: ((Input.Element) -> Bool)? = nil) {
+        self.minLength = length.lowerBound
+        self.maxLength = length.upperBound
+        self.predicate = predicate
+    }
+    
+    public init(_ length: Int, while predicate: ((Input.Element) -> Bool)? = nil) {
+        self.minLength = length
+        self.maxLength = length
+        self.predicate = predicate
+    }
+    
+    public init(_ length: PartialRangeFrom<Int>, while predicate: ((Input.Element) -> Bool)? = nil) {
+        self.minLength = length.lowerBound
+        self.maxLength = nil
+        self.predicate = predicate
+    }
+    
+    public init(_ length: PartialRangeThrough<Int>, while predicate: ((Input.Element) -> Bool)? = nil) {
+        self.minLength = 0
+        self.maxLength = length.upperBound
+        self.predicate = predicate
+    }
+    
+    public func parse(_ input: Input) -> Result<Input, Input> {
+        if let predicate = self.predicate {
+            let prefix = input.prefix(while: predicate)
+            let count = prefix.count
+            guard count >= self.minLength, self.maxLength.map({ count <= $0 }) ?? true else {
+                return .failure("Prefix not found", input)
+            }
+            return .success(prefix, input[prefix.endIndex...])
+        } else {
+            let prefix = self.maxLength.map(input.prefix) ?? input
+            let count = prefix.count
+            guard count >= self.minLength else {
+                return .failure("Prefix not found", input)
+            }
+            return .success(prefix, input[prefix.endIndex...])
+        }
+    }
+    
+}
--- Tests/ApolloTests/ApolloTests.swift
+++ Tests/ApolloTests/ApolloTests.swift
@@ -4,50 +4,6 @@ import XCTest
 final class ApolloTests: XCTestCase {
 
     func testExample() {
-        
-  
-        
-        print(levenshteinDistance(firstWord: "Łukasz", secondWord: "luk"))
-        
-        
-         struct Literal<Input, Output>: Parser where Input: Collection, Input.SubSequence == Input {
-            
-            public typealias Output = Input
-            
-            public func parse(_ input: Input) -> Result<Input, Input> {
-                .success(input, input)
-            }
-            
-        }
-//
-//        let a: Parser<Substring, Substring> = Parser.prefix(while: { !$0.isNumber })
-//
-////        print(a)
-////        let b = prefix(while: { $0.isNumber }).parse("3a")
-////        print(b)
-//        
-//        let c = Parser.prefix(while: { $0.isNumber })
-//            .zip(", ")
-//            .zip(.prefix(while: { $0.isNumber }))
-//                .parse("23a2")
-//        print(c)
-//
-//        
-//        print(a
-//                .skip(.prefix(while: { !$0.isNumber }))
-//                .zip(.prefix(while: { $0.isNumber }))
-//            .parse("23a2"))
-            
-        
-        
-//        switch prefix(while: { $0.isNumber })
-//            .flatMap({ _ in success(1) })
-//            .parse("2october1981") {
-//        case .success(let value, let remainder):
-//            print("value: \(value), remainder: \(remainder)")
-//        case .failure(let remainder):
-//            print("remainder: \(remainder)")
-//        }
     }
 
 }
